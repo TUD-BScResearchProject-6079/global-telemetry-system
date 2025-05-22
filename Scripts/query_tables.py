ebb3f3926cc592ee8bf25c5c158a5a6318b63e03
Vlad Graure <vladgraure@yahoo.ca>
2025-05-22 11:57:50 +0200

    fix typing error

--- Scripts/query_tables.py
+++ Scripts/query_tables.py
@@ -14,11 +14,13 @@ ndt_data = {'download': 'ndt7_download_case_study.csv', 'upload': 'ndt7_upload_c
 
 def prepare_data_for_case_study(conn: connection, countries: list[str]) -> None:
     country_literals = sql.SQL(', ').join(sql.Literal(c) for c in countries)
+    columns: list[str] = []
     with conn.cursor() as cur:
         for cf_table, cf_csv in cf_data.items():
             query = cf_case_study_query.format(sql.Identifier(f'cf_{cf_table}'), country_literals)
             cur.execute(query)
-            columns: list[str] = [desc[0] for desc in cur.description]
+            if cur.description is not None:
+                columns = [desc[0] for desc in cur.description]
             rows = cur.fetchall()
 
             full_path = os.path.join(path_to_save, cf_csv)
@@ -29,7 +31,8 @@ def prepare_data_for_case_study(conn: connection, countries: list[str]) -> None:
     
         query = ndt_download_case_study.format(country_literals)
         cur.execute(query)
-        columns = [desc[0] for desc in cur.description]
+        if cur.description is not None:
+            columns = [desc[0] for desc in cur.description]
         rows = cur.fetchall()
         full_path = os.path.join(path_to_save, ndt_data['download'])
         with open(full_path, 'w', newline='', encoding='utf-8') as f:
@@ -39,7 +42,8 @@ def prepare_data_for_case_study(conn: connection, countries: list[str]) -> None:
         
         query = ndt_upload_case_study.format(country_literals)
         cur.execute(query)
-        columns = [desc[0] for desc in cur.description]
+        if cur.description is not None:
+            columns = [desc[0] for desc in cur.description]
         rows = cur.fetchall()
         full_path = os.path.join(path_to_save, ndt_data['upload'])
         with open(full_path, 'w', newline='', encoding='utf-8') as f:
@@ -48,7 +52,9 @@ def prepare_data_for_case_study(conn: connection, countries: list[str]) -> None:
                 writer.writerows(rows)
 
 def get_top_five_isps_countries_with_starlink_measurements(conn: connection)-> None:
+     columns: list[str] = []
      with conn.cursor() as cur:
         cur.execute(top_five_isps_countries_with_starlink_measurements)
-        columns = [desc[0] for desc in cur.description]
+        if cur.description is not None:
+            columns = [desc[0] for desc in cur.description]
         rows = cur.fetchall()
